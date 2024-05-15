19cd304e7b5605273a1c7d966faa6fd4db885f7f
jayjithendra <70255332+jayjithendra@users.noreply.github.com>
2024-05-15 15:24:39 +0530

    Add files via upload

---
+++ Lab2/Banking Application/Banking Application/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_Application
+{
+    public partial class Form1 : Form
+    {
+        string userid = "JayJ27";
+        string pwdog = "student";
+        
+        public Form1()
+        {
+            InitializeComponent();
+            label7.Visible = false;
+            label6.Visible = false;
+            label5.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pwdog != textBox2.Text || userid != textBox1.Text)
+            {
+                label3.Text = "Login Unsuccessful";
+            }
+            else
+            {
+                label3.Text = "Login Successful";
+                Form2 frm = new Form2();
+                frm.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            label7.Visible = true;
+            label6.Visible = true;
+            label5.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            button2.Visible = true;
+            if (pwdog == textBox4.Text)
+            {
+                pwdog = textBox3.Text;
+                string message = "Password Updated";
+                MessageBox.Show(message);
+            }
+            else
+            {
+                label7.Text = "Old Password does not match";
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label7.Visible = true;
+            label6.Visible = true;
+            label5.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            button2.Visible = true;
+            if (pwdog == textBox4.Text)
+            {
+                pwdog = textBox3.Text;
+                string message = "Password Updated";
+                MessageBox.Show(message);
+            }
+            else
+            {
+                label7.Text = "Old Password does not match";
+            }
+
+        }
+    }
+}
---
+++ Lab2/Lab2.1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog(); dlg.Title = "Open";
+            dlg.ShowDialog();
+            string fName = dlg.FileName;
+            StreamReader sr = new StreamReader(fName); richTextBox1.Text = sr.ReadToEnd(); sr.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string fName = saveFileDialog1.FileName; StreamWriter sw = new StreamWriter(fName); sw.Write(richTextBox1.Text);
+            sw.Flush();
+            sw.Close();
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog(); 
+            fd.Font = richTextBox1.SelectionFont; 
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = richTextBox1.SelectionColor;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = cd.Color;
+            }
+        }
+    }
+}
